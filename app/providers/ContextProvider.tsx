cef0390bf4cc4b741c21248eb01a4b3f4c55eb76
Ayumu0922 <9bota.ayu6@yahoo.ne.jp>
2024-02-22 00:34:06 +0900

    create provider and context

---
+++ app/providers/ContextProvider.tsx
@@ -0,0 +1,26 @@
+"use client";
+
+import React, { useEffect, useState } from "react";
+import { GlobalProvider } from "../context/globalProvider";
+
+interface Props {
+  children: React.ReactNode;
+}
+
+function ContextProvider({ children }: Props) {
+  const [isReady, setIsReady] = useState(false);
+
+  // 設定を適切に反映するための処理
+  useEffect(() => {
+    setTimeout(() => {
+      setIsReady(true);
+    }, 200);
+  }, []);
+
+  if (!isReady) {
+    return null;
+  }
+  return <GlobalProvider>{children}</GlobalProvider>;
+}
+
+export default ContextProvider;
